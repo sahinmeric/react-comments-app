6af836b066d33f657386c7ecb5c8f12f0a73e8f7
sahinmeric[201~ <sahin.meric86@gmail.com>
2022-11-24 20:16:49 -0300

    useParam and data filtering integrated

--- src/CommentDetail.tsx
+++ src/CommentDetail.tsx
@@ -1,11 +1,14 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useComments } from "./Comments";
 
 function CommentDetail() {
     const navigate = useNavigate()
-
+    const { commentId } = useParams();
+    const { data: commentData } = useComments();
+    const comment = commentData?.find((comment) => comment.id === Number(commentId))
     return (
         <div>
-            <p>This is comment detail page</p>
+            <p>{comment?.body}</p>
             <button onClick={() => navigate(-1)}>Back</button>
         </div>
     )
