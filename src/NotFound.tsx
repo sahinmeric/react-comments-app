d02b3de72961539b5f944d4e863ca9e0ce89f2fa
sahinmeric[201~ <sahin.meric86@gmail.com>
2022-11-26 18:11:10 -0300

    visual improvements with mui

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Box, Button, Typography } from '@mui/material';
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
-function NotFound() {
+
+function NotFoundNew() {
     const navigate = useNavigate()
-
-    useEffect(() => {
-        setTimeout(() => {
-            navigate("/")
-        }, 3000)
-    }, [navigate])
     return (
-        <div>
-            <h1>Page not found!</h1>
-            <h4>Returning to main page...</h4>
-        </div>
-    )
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                minHeight: '100vh',
+                backgroundColor: '#C6EBEB',
+            }}
+        >
+            <Typography variant="h1" style={{ color: 'black' }}>
+                404
+            </Typography>
+            <Typography variant="h6" style={{ color: 'black' }}>
+                The page you’re looking for doesn’t exist.
+            </Typography>
+            <Button
+                variant="contained"
+                size="small"
+                color="error"
+                startIcon={<ArrowBackIosIcon />}
+                onClick={() => navigate(-1)}>
+                BACK
+            </Button>
+        </Box>
+    );
 }
 
-export default NotFound;
+export default NotFoundNew;
