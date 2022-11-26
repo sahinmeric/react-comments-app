3fd5955bc85886a0863a67b030450e973470fb86
sahinmeric[201~ <sahin.meric86@gmail.com>
2022-11-26 18:13:14 -0300

    some visual improvements on circularprogress animation

--- src/ShowProgress.tsx
+++ src/ShowProgress.tsx
@@ -3,7 +3,12 @@ import { Box, CircularProgress } from '@mui/material';
 
 function ShowProgress() {
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Box style={{
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)"
+        }}>
             <CircularProgress />
         </Box>
     );
