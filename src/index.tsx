7105ad3d6623a1107f4f1b6312a7146d4ddc8b72
sahinmeric[201~ <sahin.meric86@gmail.com>
2022-11-26 18:20:21 -0300

    index css added

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom"
+
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 
 root.render(
